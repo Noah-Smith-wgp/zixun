44d52c90561e66ee60fb363c08725abe58ae514f
Noah-Smith-wgp <18211672297@163.com>
2020-01-12 14:25:50 +0800

    配置代码抽取

--- manage.py
+++ manage.py
@@ -1,23 +1,7 @@
-# import redis
-# from flask import Flask
-# from flask_session import Session
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_wtf import CSRFProtect
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from project import app, db
-
-# from config import Config
-#
-# app = Flask(__name__)
-#
-#
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# CSRFProtect(app)
-# Session(app)
 
 manager = Manager(app)
 Migrate(app, db)
