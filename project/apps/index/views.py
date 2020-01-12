ecbb4d9f290cb2e118cbc6defbad9b800b9d9a59
Noah-Smith-wgp <18211672297@163.com>
2020-01-12 17:08:52 +0800

    创建模板文件夹

--- project/apps/index/views.py
+++ project/apps/index/views.py
@@ -1,6 +1,13 @@
 from . import index_buleprint
+from flask import render_template, current_app
+
+
+# 网站图标展示
+@index_buleprint.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
 
 
 @index_buleprint.route('/')
 def index():
-    return 'index~~'
+    return render_template('news/index.html')
