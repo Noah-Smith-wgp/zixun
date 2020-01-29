43eadf6d7411e43e1b6161c49eed4ed13e0b8761
Noah-Smith-wgp <18211672297@163.com>
2020-01-29 15:30:30 +0800

    基类模板抽取

--- project/apps/news/views.py
+++ project/apps/news/views.py
@@ -5,4 +5,6 @@ from . import news_blueprint
 
 @news_blueprint.route('/<int:news_id>')
 def news_detail(news_id):
-    return render_template('news/detail.html')
+
+    data = {}
+    return render_template('news/detail.html', data=data)
