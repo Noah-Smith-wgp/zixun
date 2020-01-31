5770d3c63cf91f5cbc5f4e8c4250ef7b0cf6641f
Noah-Smith-wgp <18211672297@163.com>
2020-01-31 17:49:32 +0800

    基本资料设置

--- project/apps/user/views.py
+++ project/apps/user/views.py
@@ -1,10 +1,8 @@
-from flask import g, redirect, render_template
-from flask_restful import Api, Resource
+from flask import g, redirect, render_template, request, jsonify, current_app, session
 
-from project import user_login_data
+from project import user_login_data, db
+from project.utils.response_code import RET
 from . import user_blueprint
-
-user_api = Api(user_blueprint)
 
 
 @user_blueprint.route('/info')
@@ -21,3 +19,43 @@ def get_user_info():
     }
     # 渲染模板
     return render_template("news/user.html", data=data)
+
+
+@user_blueprint.route('/base_info', methods=["GET", "POST"])
+@user_login_data
+def base_info():
+    """
+    用户基本信息
+    :return:
+    """
+    user = g.user
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', data={"user_info": user.to_dict()})
+
+    # 2. 获取到传入参数
+    data_dict = request.json
+    nick_name = data_dict.get("nick_name")
+    gender = data_dict.get("gender")
+    signature = data_dict.get("signature")
+    if not all([nick_name, gender, signature]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
+
+    if gender not in (['MAN', 'WOMAN']):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
+
+    # 3. 更新并保存数据
+    user.nick_name = nick_name
+    user.gender = gender
+    user.signature = signature
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存数据失败")
+
+    # 将 session 中保存的数据进行实时更新
+    session["nick_name"] = nick_name
+
+    # 4. 返回响应
+    return jsonify(errno=RET.OK, errmsg="更新成功")
