58ec4a9c3b627c98d1d6d0d3d2fc38e486c1a1e3
Noah-Smith-wgp <18211672297@163.com>
2020-02-01 11:21:48 +0800

    创建管理员用户

--- manage.py
+++ manage.py
@@ -3,10 +3,34 @@ from flask_migrate import Migrate, MigrateCommand
 
 from project import app, db
 from project.models import models
+from project.models.models import User
 
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.option('-n', '-name', dest='name')
+@manager.option('-p', '-password', dest='password')
+def createsuperuser(name, password):
+    """创建管理员用户"""
+    if not all([name, password]):
+        print('参数不足')
+        return
+
+    user = User()
+    user.mobile = name
+    user.nick_name = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        print("创建成功")
+    except Exception as e:
+        print(e)
+        db.session.rollback()
 
 
 if __name__ == '__main__':
